9d03af3a3854dff6c230a44ea3d05361b095e489
reakrakover <reak@gefen.online>
2018-10-23 17:36:30 +0300

    stage 3

--- test.py
+++ test.py
@@ -2,13 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.soccervista.com/bet.php"
-home_selector = "#col1"
-game_link_selector = "#col1 > div:nth-of-type(20) > table:nth-of-type(3) > tbody > tr.onem > td.detail > a"
-
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "html.parser")
-link = soup.select("#search-results-list > li:nth-child(1) > div > div > div.col-xs-8.col-sm-9.card-meta-data > div.title-author-info > h3 > a")
 
+# ---------------------------------------------------------
 draw_first_appear = str(soup).find("DRAW bet of the day")
 slice_one = str(soup)[draw_first_appear:]
 
@@ -20,8 +17,8 @@ html_first_appear = slice_two.find(".html")
 slice_three = slice_two[href_first_appear+6:html_first_appear+5]
 
 final_url = "https://www.soccervista.com/{}".format(slice_three)
-
 print(final_url)
+# ---------------------------------------------------------
 
 r = requests.get(final_url)
 soup = BeautifulSoup(r.content, "html.parser")
