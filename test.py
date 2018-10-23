b44116f8a33c49630f4bf3a5c7d4c2b69be8ebe0
reakrakover <reak@gefen.online>
2018-10-23 23:34:46 +0300

    final script with gspread

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+# ---------------------------------------------------------
 url = "https://www.soccervista.com/bet.php"
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "html.parser")
-
 # ---------------------------------------------------------
+
 draw_first_appear = str(soup).find("DRAW bet of the day")
 slice_one = str(soup)[draw_first_appear:]
 
@@ -17,7 +20,6 @@ html_first_appear = slice_two.find(".html")
 slice_three = slice_two[href_first_appear+6:html_first_appear+5]
 
 final_url = "https://www.soccervista.com/{}".format(slice_three)
-print(final_url)
 # ---------------------------------------------------------
 
 r = requests.get(final_url)
@@ -25,4 +27,14 @@ soup = BeautifulSoup(r.content, "html.parser")
 score = soup.find_all("td", {"class": "score"})
 
 for i in score:
-    print(i.text)
+    final_score = i.text
+
+# --------------------------------------
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+# --------------------------------------
+sheet = client.open("soccervista").sheet1
+row = [final_url, final_score]  # can add a list with elements separated with comma for each cell
+index = 2  # the row number we want to add and update
+sheet.insert_row(row, index)
